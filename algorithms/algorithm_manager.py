90436a46dd4a2e9b86e10d701795a0ef0e5f393d
Alex Jasson <alexjasson94@gmail.com>
2025-03-15 14:11:45 +1100

    implement data import

--- algorithms/algorithm_manager.py
+++ algorithms/algorithm_manager.py
@@ -8,9 +8,9 @@ def fastICA(otb_filepath, save_dir, output_filename, to_filter=True):
     
     Parameters:
       otb_filepath (str): Path to the .otb file.
-      save_dir (str): Directory for saving temporary and output files.
-      output_filename (str): Path (including filename) where the results will be saved.
-      to_filter (bool): Whether to perform notch and bandpass filtering.
+      save_dir (str): directory at which final discharges will be saved
+      output_filename (str): output file name
+      to_filter (bool): whether or not you notch and butter filter the data
     """
     # Create an instance of the offline_EMG class
     processor = offline_EMG(save_dir, to_filter)
@@ -18,11 +18,10 @@ def fastICA(otb_filepath, save_dir, output_filename, to_filter=True):
     # Read and extract the .otb file and parse metadata
     processor.open_otb(otb_filepath)
     
-    # Perform the decomposition
+    # Perform the decomposition here...
 
     # FastICA algorithm here...
-    
+
     # write to output_filename here...
 
-    
-    print(f"Processing complete. Results saved to {output_filename}")
+    # Save results to save_dir here ...
