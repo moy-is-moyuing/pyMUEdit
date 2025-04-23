1e9526644bb0fabeedfcce49370d5e936960b961
4ForksSake <oli@rmbgroup.com.au>
2025-04-23 14:33:15 +1000

    Updated VisualisationPage.py
    Better wrapping with ChannelViewer.py

--- src/ui/components/VisualisationPage.py
+++ src/ui/components/VisualisationPage.py
@@ -4,16 +4,11 @@ from app.gui.components.VisualizationPanel import VisualizationPanel
 from app.gui.components.ChannelViewer import ChannelViewer
 
 class VisualisationPage(QWidget):
-    def __init__(self, emg_obj, parent=None):
+    def __init__(self, emg_data, parent=None):
         super().__init__(parent)
-
         layout = QVBoxLayout()
-
-        # Get matrix from loaded signal dict
-        emg_data = emg_obj.signal_dict["data"]
-
         viewer = ChannelViewer(emg_data)
         vis_panel = VisualizationPanel(title="EMG Channel Viewer", plot_widget=viewer)
-
         layout.addWidget(vis_panel)
         self.setLayout(layout)
+
