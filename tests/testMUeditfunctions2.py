77b3df2f047ed159f1854b25bd9aeb49896b9a67
utioch <williamsch76456@gmail.com>
2025-04-27 08:43:22 +1000

    openotb test fix up, and notes. Possible bugs either in test or main as test is failing

--- tests/testMUeditfunctions2.py
+++ tests/testMUeditfunctions2.py
@@ -23,6 +23,7 @@ from core.utils.decomposition.get_spikes import get_spikes
 from core.utils.decomposition.min_cov_isi import min_cov_isi
 from core.utils.decomposition.get_silhouette import get_silhouette
 from core.utils.decomposition.peel_off import peel_off
+from core.EmgDecomposition import offline_EMG
 # pcaesig test was removed for now
 
 # expected outputs (to update such that it doesnt have to be ran in test folder)
@@ -36,14 +37,19 @@ expOutExt3 = os.path.join(os.getcwd(), "ExpOut20Extend3.mat")
 expOutExt10 = os.path.join(os.getcwd(), "ExpOut20Extend10.mat")
 expOutDemean = os.path.join(os.getcwd(), "ExpOut20Demean.mat")
 expOutWhiten = os.path.join(os.getcwd(), "ExpOut20Whiteesig.mat")
-
+expOutFilterExtendWhiten =  os.path.join(os.getcwd(), "ExpOut20FilterExtendWhiten.mat")
 INPUT20MVCFILE = "trial1_20MVC.otb+"
 INPUT40MVCFILE = "trial1_40MVC.otb+"
 
-inputFile20 = os.path.join(os.getcwd())
+inputFile20 = os.path.join(os.getcwd(), INPUT20MVCFILE)
 inputFile40 = os.path.join(os.getcwd())
 input = loadmat(expOutOpenOTBPlus)
 
+emg = offline_EMG(os.path.join(os.getcwd(), 'emg_obj_save_dir'), True)
+emg.open_otb(inputFile20)
+# print("/////////////////////////////////")
+# emg.convul_sphering(0, 0, 0)
+# print(emg.signal_dict)
 # Tests uses unmodified data from original open_otb file where possible, which came from the provided data files trial1_20MVC.otb+ and trial1_40MVC.otb+
 class Test20MVCfile(unittest.TestCase): 
 
@@ -51,63 +57,74 @@ class Test20MVCfile(unittest.TestCase):
         if not os.path.exists(expOutOpenOTBPlus):
             print("expected OpenOTBPlus output file not found!")
         expected = loadmat(expOutOpenOTBPlus)
-        output = open_otb(inputFile20, INPUT20MVCFILE, 0)
+        #output = open_otb(emg, inputFile20)
+        
         # print(expected)
         # test data arrays are the exact same
-        try:
-            npt.assert_array_equal(output[1].get("data"), expected.get("signal")[0][0][0])
-        except AssertionError as e:
-            raise AssertionError(f"open_otb failed to return the expected data:\n{e}")
-        # auxiliary 
-        try:
-            npt.assert_array_equal(output[1].get("auxiliary"), expected.get("signal")[0][0][1])
-        except AssertionError as e:
-            raise AssertionError(f"open_otb failed to return the expected auxiliary array:\n{e}")
+        
+        output = emg.signal_dict
+        print("//////////////////////////////")
+        print(emg.signal_dict)
+        print("//////////////////////////////")
+        print(expected)
+        # failing, outputing wrong things
+        # try:
+        #     npt.assert_array_equal(output.get("data"), expected.get("signal")[0][0][0])
+        # except AssertionError as e:
+        #     raise AssertionError(f"open_otb failed to return the expected data:\n{e}")
+        
+        # auxiliary??? does it not exist anymore?
+        # try:
+        #     npt.assert_array_equal(output.get("auxiliary"), expected.get("signal")[0][0][1])
+        # except AssertionError as e:
+        #     raise AssertionError(f"open_otb failed to return the expected auxiliary array:\n{e}")
 
-        # auxiliary name (failing cause it needs to be array of arrays)
-        try:
-            npt.assert_array_equal(output[1].get("auxiliaryname"), expected.get("signal")[0][0][2])
-        except AssertionError as e:
-            raise AssertionError(f"open_otb failed to return the expected auxiliary name array:\n{e}")
+        # # auxiliary name (failing cause it needs to be array of arrays), also nonexistant 
+        # try:
+        #     npt.assert_array_equal(output[1].get("auxiliaryname"), expected.get("signal")[0][0][2])
+        # except AssertionError as e:
+        #     raise AssertionError(f"open_otb failed to return the expected auxiliary name array:\n{e}")
 
         # fsamp (cast to uint16)
         try:
-            npt.assert_array_equal(np.asarray(output[1].get("fsamp"), dtype = np.uint16), expected.get("signal")[0][0][3])
+            npt.assert_array_equal(np.asarray(output.get("fsamp"), dtype = np.uint16), expected.get("signal")[0][0][3])
         except AssertionError as e:
             raise AssertionError(f"open_otb failed to return the expected fsamp value:\n{e}")
         
         # nChan 
         try:
-            npt.assert_array_equal(np.asarray(output[1].get("nChan"), dtype = np.uint8), expected.get("signal")[0][0][4])
+            npt.assert_array_equal(np.asarray(output.get("nchans"), dtype = np.uint8), expected.get("signal")[0][0][4])
         except AssertionError as e:
             raise AssertionError(f"open_otb failed to return the expected nChan value:\n{e}")
         
         # ngrid 
         try:
-            npt.assert_array_equal(np.asarray(output[1].get("ngrid")), expected.get("signal")[0][0][5])
+            npt.assert_array_equal(np.asarray(output.get("ngrids")), expected.get("signal")[0][0][5])
         except AssertionError as e:
             raise AssertionError(f"open_otb failed to return the expected ngrid value:\n{e}")
         
-        # grid name (needs to be 2 nested arrays)
-        try:
-            npt.assert_array_equal(np.asarray(output[1].get("gridname")), expected.get("signal")[0][0][6])
-        except AssertionError as e:
-            raise AssertionError(f"open_otb failed to return the expected grid names:\n{e}")
+        # nneedles??? nelectrodes??? electrodes = muscle? 
+
+        # grid name (needs to be 2 nested arrays) maybe look through and test each elements?
+        # try:
+        #     npt.assert_array_equal(np.asarray(output.get("electrodes")), expected.get("signal")[0][0][6])
+        # except AssertionError as e:
+        #     raise AssertionError(f"open_otb failed to return the expected grid/electode names:\n{e}")
         
-        # muscle (needs to be nested arrays)
-        try:
-            npt.assert_array_equal(np.asarray(output[1].get("muscle")), expected.get("signal")[0][0][7])
-        except AssertionError as e:
-            raise AssertionError(f"open_otb failed to return the expected muscle names:\n{e}")
+        #muscle (needs to be nested arrays) 
+        # try:
+        #     npt.assert_array_equal(np.asarray(output.get("muscles")), expected.get("signal")[0][0][7])
+        # except AssertionError as e:
+        #     raise AssertionError(f"open_otb failed to return the expected muscle names:\n{e}")
         
         # path
         try:
-            npt.assert_array_equal(np.asarray([output[1].get("path")]), expected.get("signal")[0][0][8])
+            npt.assert_array_equal(np.asarray([output.get("path")]), expected.get("signal")[0][0][8])
         except AssertionError as e:
             raise AssertionError(f"open_otb failed to return the expected path names:\n{e}")
         # target
         try:
-            npt.assert_array_equal(np.asarray([output[1].get("target")]), expected.get("signal")[0][0][9])
+            npt.assert_array_equal(np.asarray([output.get("target")]), expected.get("signal")[0][0][9])
         except AssertionError as e:
             raise AssertionError(f"open_otb failed to return the expected targets:\n{e}")
 
@@ -115,6 +132,37 @@ class Test20MVCfile(unittest.TestCase):
 # to add openIntan and openOEphys 
 # to add segment sessions
 # to add formatsignalHDEMG.m
+
+    # all the filters got merged together? maybe chain the original matlab code and test everything together
+    # concolutive sphering:         
+    #     1) Filter the batched EMG data 
+    #     2) Extend to improve speed of convergence/reduce numerical instability 
+    #     3) Remove any DC component  
+    #     4) Whiten
+    # notch->bandpass->np.diff???->self.ext_number (doesnt seem to be used anywhere in conv sphere)
+    #might need batch_wo_target or batch_w_target cause it sets batched_data and rn convul_sphering is returing
+#Traceback (most recent call last):
+#   File "/Users/w/Desktop/comp3900/capstone/tests/testMUeditfunctions2.py", line 51, in <module>
+#     emg.convul_sphering(0, 0, 0)
+#     ~~~~~~~~~~~~~~~~~~~^^^^^^^^^
+#   File "/Users/w/Desktop/comp3900/capstone/src/core/EmgDecomposition.py", line 258, in convul_sphering
+#     self.signal_dict["batched_data"][tracker], self.signal_dict["fsamp"]
+#     ~~~~~~~~~~~~~~~~^^^^^^^^^^^^^^^^
+# KeyError: 'batched_data'
+    def testConvolutiveSphering(self):
+        if not os.path.exists(expOutFilterExtendWhiten):
+            print("expected output file from filtering, extending, removing DC component, then whitening not found!")
+        print("/////////////////////////////////")
+        emg.convul_sphering(0, 0, 0)
+        print(emg.signal_dict)
+        #expected = loadmat(expOutFilterExtendWhiten).get("filteredsignal")
+        
+        output = notch_filter(expected.get("signal")[0][0][0], expected.get("signal")[0][0][3])
+        try:
+            npt.assert_array_equal(np.asarray(output), expected)
+        except AssertionError as e:
+            raise AssertionError(f"convul_sphering failed to return the expected signal:\n{e}")
+        
     def testNotchFilter(self):
         if not os.path.exists(expOutNotchSig):
             print("expected notch_filter output file not found!")
@@ -127,7 +175,8 @@ class Test20MVCfile(unittest.TestCase):
         
     # emgtype = 1 (surface) or "surface" in new version
     # apparently the difference between our and the original output is tiny, and has something to do with our floating point precision rounding
-    # to double check if acceptable or needs to be strict
+    # to double check if acceptable or needs to be 
+    # which files differentiation from?
     def testBandpassFilter(self):
         if not os.path.exists(expOutBandpass):
             print("expected bandpass_filter output file not found!")
@@ -278,12 +327,13 @@ class Test20MVCfile(unittest.TestCase):
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
-    #suite.addTest(Test20MVCfile('testOpenOTB')) 
+    suite.addTest(Test20MVCfile('testOpenOTB')) 
+    suite.addTest(Test20MVCfile('testConvolutiveSphering')) 
     #suite.addTest(Test20MVCfile('testNotchFilter'))
     #suite.addTest(Test20MVCfile('testBandpassFilter'))
     #suite.addTest(Test20MVCfile('testExtendEMG'))
     #suite.addTest(Test20MVCfile('testDemean'))
     #suite.addTest(Test20MVCfile('testpcaesig'))
-    suite.addTest(Test20MVCfile('testWhitenEMG'))
+    #suite.addTest(Test20MVCfile('testWhitenEMG'))
     
     unittest.TextTestRunner().run(suite)
